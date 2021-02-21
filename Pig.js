f557b249188692df170e41372a3ad3a0ae3716e9
L2442 <76606422+L2442@users.noreply.github.com>
2021-02-21 09:51:22 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,21 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    console.log(this.body);
+    this.visibility = 255;
+  }
+display(){
+  if(this.body.speed < 3){
+    super.display();
+  }
+  else{
+    push();
+    tint(255,this.visibility);
+    this.visibility = this.visibility-5;
+    image(this.image, this.body.position.x, this.body.position.y, 50,50);
+    pop();
+    World.remove(world, this.body);
+  }
+}
+};
